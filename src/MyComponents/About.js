7ed2e920005119add15e278cc6f51cbdf7abfedf
Saahil <saahilkumarjha@gmail.com>
2021-11-16 10:04:40 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h3>This is "about" component</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Debitis nemo ad sequi? Ea sunt sint tenetur, aspernatur 
+          possimus pariatur recusandae quia commodi. Inventore ullam 
+          itaque minus dolore officia nostrum dignissimos eaque dolorem 
+          cupiditate assumenda.</p>
+    </div>
+  )
+}
